6a045bf834b4e5a9737f7ef80e15a1ce8b9f48a2
Hendra Eddy <hendra_eddy@ymail.com>
2022-03-08 01:53:51 +0700

    Initial
    Signed-off-by: Hendra Eddy <hendra_eddy@ymail.com>

---
+++ README.md
@@ -0,0 +1,76 @@
+# SSH for remote SMB Directory with Laravel Vallet Usage
+
+This is application for private usage, but if you want use this apps then you can download.
+
+## Features
+
+This need putty/ssh installed on your computer.
+
+I'm test in windows and this work for me.
+
+I'm not test in other operating system like Linux, Mac and Other.
+
+For usage this extension:
+
+<!-- \!\[feature X\]\(images/feature-x.png\) -->
+
+> Usage: Please install putty/ssh in your computer and you open the setting then search SSH, you need configure this extension before you use this extension.
+
+## Requirements
+
+- Install putty/ssh on your computer.
+- This tested on Windows only.
+
+## Extension Settings
+
+You need configure before use this extension `sshSmbFolder.connection` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `sshSmbFolder.connection.name`: write something for the name connection.
+* `sshSmbFolder.connection.user`: write your username for connect the remote server via SSH.
+* `sshSmbFolder.connection.password`: write your password for connect the remote server via SSH.
+* `sshSmbFolder.connection.host`: write your IP-Address or URL for connect the remote server via SSH.
+* `sshSmbFolder.connection.port`: write your Open-port for connect the remote server via SSH.
+* `sshSmbFolder.connection.enableKeyFile`: Enable/disable for connect SSH with some keyFile you create on the server.
+* `sshSmbFolder.connection.keyFile`: write your path included name of keyFile on your computer drive.
+* `sshSmbFolder.connection.prefixPath`: write your path folder in remote server, where you use SMB Folder active.
+* `sshSmbFolder.connection.delay`: write your delay for connecting the remote server before the command use in terminal automatic like for Login and change path for your terminal.
+
+## Known Issues
+
+Calling me if you have idea and have some code for compability the other operating system.
+
+But maybe i'm not fast response or response your call if not insterested.
+
+## Release Notes
+
+This first release for extension.
+
+### 1.0.0
+
+Initial release of ...
+
+-----------------------------------------------------------------------------------------------------------
+## Following extension guidelines
+
+Ensure that you've read through the extensions guidelines and follow the best practices for use this extension.
+
+<!-- * [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+
+## Working with Markdown
+
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets -->
+
+### For more information
+
+* [Hendra Eddy](https://github.com/hendraedi/)
+* [Email to Hendra Eddy](mailto:hendra_eddy@ymail.com)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,90 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as fs from "fs";
+import { isNumber } from 'util';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "ssh-smb-folder" is now active!');
+	
+	const terminal = vscode.window.createTerminal();
+	let config = vscode.workspace.getConfiguration('sshSmbFolder.connection');
+	// let delayEx = 0;
+	const name = config.get('name');
+	const user = config.get('user');
+	const password = config.get('password');
+	const host = config.get('host');
+	const port = config.get('port');
+	const enableKeyFile = config.get('enableKeyFile');
+	const keyFile = config.get('keyFile');
+	const prefixPath = config.get('prefixPath');
+	const delayEx = Number (config.get('delay'));
+	let connecting = false;
+	console.log('Data: '+ JSON.stringify(config));
+	let pathFolder = "";
+
+	if(vscode.workspace.workspaceFolders !== undefined) {
+		let full = vscode.workspace.workspaceFolders[0].uri.path ;
+
+		let fPath = full.substring(4);
+		pathFolder = fPath;
+		console.log(" | fspath: " + fPath);
+	}
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('ssh-smb-folder.connect', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Start connecting to remote SMB Folder via SSH, Please wait..!');
+
+		if(!connecting){
+			terminal.show();
+			if(enableKeyFile === true){
+				let command = "ssh " + "-i " + keyFile + " " + user + "@" + host + " -p " + port;
+				terminal.sendText(command);
+			} else {
+				let command = "ssh " + user + "@" + host + " -p " + port;
+				terminal.sendText(command);
+			}
+			let delayEx = 0;
+			
+			inPass(delayEx);
+			async function inPass(second: number) {
+				for (let index = 0; index < second; index++) {
+					console.log(index);
+					await sleep(1000);
+					if(index === (second - 1)){
+						terminal.sendText(""+password);
+						inDir(delayEx);
+					}
+				}
+			}
+			
+			async function inDir(second: number) {
+				for (let index = 0; index < second; index++) {
+					console.log(index);
+					await sleep(1000);
+					if(index === (second - 1)){
+						terminal.sendText("cd "+ prefixPath + "/" + pathFolder);
+					}
+				}
+			}
+
+			function sleep(ms: number | undefined) {
+				return new Promise((resolve) => {
+					setTimeout(resolve, ms);
+				});
+			}
+		}
+	});
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
